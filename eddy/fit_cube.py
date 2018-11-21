d35c757d6b3370e8c21934905e2ceff365a6beb6
richteague <richarddteague@gmail.com>
2018-11-21 17:28:13 -0500

    everything is at least working...

--- eddy/fit_cube.py
+++ eddy/fit_cube.py
@@ -13,17 +13,24 @@ import scipy.constants as sc
 
 class rotationmap:
 
-    def __init__(self, path, error=None, clip=None, downsample=None):
+    def __init__(self, path, uncertainty=None, clip=None, downsample=None):
         """Initialize."""
 
         # Read in the data and position axes.
         self.data = fits.getdata(path)
         self.header = fits.getheader(path)
         try:
-            self.error = fits.getdata(error)
+            self.error = fits.getdata(uncertainty)
         except:
             print("No uncertainties found, assuming uncertainties of 10%.")
             self.error = 0.1 * self.data
+        self.error = np.where(np.isnan(self.error), 0.0, self.error)
+
+        # Make sure this is in [km/s].
+        if np.nanmedian(self.data) > 10.0:
+            self.data /= 1e3
+            self.error /= 1e3
+
         self.xaxis = self._read_position_axis(a=1)
         self.yaxis = self._read_position_axis(a=2)
         self.dpix = abs(np.diff(self.xaxis)).mean()
@@ -33,6 +40,7 @@ class rotationmap:
             self._clip_cube(clip)
         if downsample is not None:
             self._downsample_cube(downsample)
+        self.mask = np.isfinite(self.data)
 
         # Estimate the systemic velocity.
         self.vlsr = np.nanmedian(self.data)
@@ -80,11 +88,14 @@ class rotationmap:
         # Calculate the inverse variance mask.
         r_min = r_min if r_min is not None else 0.0
         r_max = r_max if r_max is not None else 1e5
-        self.ivar = self._calculate_ivar(x0=params['x0'], y0=params['y0'],
-                                         inc=params['inc'], PA=params['PA'],
-                                         z0=params['z0'], psi=params['psi'],
-                                         tilt=params['tilt'], r_min=r_min,
-                                         r_max=r_max)
+
+        temp = rotationmap._populate_dictionary(p0, params)
+        self.ivar = self._calc_ivar(x0=temp['x0'], y0=temp['y0'],
+                                    inc=temp['inc'], PA=temp['PA'],
+                                    z0=temp['z0'], psi=temp['psi'],
+                                    tilt=temp['tilt'], r_min=r_min,
+                                    r_max=r_max)
+        self.plot_data(ivar=self.ivar)
 
         # Run an initial optimization using scipy.minimize.
         if optimize:
@@ -103,7 +114,7 @@ class rotationmap:
         p0 = rotationmap._random_p0(p0, scatter, nwalkers)
 
         sampler = emcee.EnsembleSampler(nwalkers, ndim, self._ln_probability,
-                                        args=(params))
+                                        args=[params, np.nan])
         sampler.run_mcmc(p0, nburnin + nsteps)
         samples = sampler.chain[:, -int(nsteps):]
         samples = samples.reshape(-1, samples.shape[-1])
@@ -112,7 +123,7 @@ class rotationmap:
         if plot_walkers:
             rotationmap.plot_walkers(sampler.chain.T, nburnin, labels)
         if plot_corner:
-            functions.plot_corner(samples, labels)
+            rotationmap.plot_corner(samples, labels)
 
         return
 
@@ -126,15 +137,16 @@ class rotationmap:
 
     def _ln_likelihood(self, params):
         """Log-likelihood function. Simple chi-squared likelihood."""
-        lnx2 = np.power((self.data - self._make_model(params)), 2)
+        model = self._make_model(params) * 1e-3
+        lnx2 = np.where(self.mask, np.power((self.data - model), 2), 0.0)
         lnx2 = -0.5 * np.sum(lnx2 * self.ivar)
         return lnx2 if np.isfinite(lnx2) else -np.inf
 
-    def _ln_probability(self, theta, params):
+    def _ln_probability(self, theta, *params_in):
         """Log-probablility function."""
-        params = rotationmap._populate_dictionary(theta, params)
-        if np.isfinite(self._ln_priors(params)):
-            return self._ln_likelihood(params)
+        model = rotationmap._populate_dictionary(theta, params_in[0])
+        if np.isfinite(self._ln_prior(model)):
+            return self._ln_likelihood(model)
         return -np.inf
 
     def _ln_prior(self, params):
@@ -149,23 +161,23 @@ class rotationmap:
             return -np.inf
         if not 0.0 < params['mstar'] < 5.0:
             return -np.inf
-        if abs(self.vlsr - params['vlsr'] * 1e3) > 1e3:
+        if abs(self.vlsr - params['vlsr'] / 1e3) > 1.0:
             return -np.inf
-        if not 0. <= params['z0'] < 1.0:
+        if not 0.0 <= params['z0'] < 1.0:
             return -np.inf
-        if not 0. < params['psi'] < 2.0:
+        if not 0.0 <= params['psi'] < 2.0:
             return -np.inf
         if not -1.0 < params['tilt'] < 1.0:
             return -np.inf
         return 0.0
 
-    def _calc_ivar(self, x0=0.0, y0=0.0, inc=None, PA=0.0, z0=0.0, psi=0.0,
+    def _calc_ivar(self, x0=0.0, y0=0.0, inc=0.0, PA=0.0, z0=0.0, psi=0.0,
                    tilt=0.0, r_min=0.0, r_max=1e5):
         """Calculate the inverse variance including radius mask."""
         rvals = self.disk_coords(x0=x0, y0=y0, inc=inc, PA=PA,
                                  z0=z0, psi=psi, tilt=tilt)[0]
         mask = np.logical_and(rvals >= r_min, rvals <= r_max)
-        mask = np.logical_and(mask, np.isfinite(self.data))
+        mask = np.logical_and(mask, self.error > 0.0)
         return np.where(mask, np.power(self.error, -2.0), 0.0)
 
     @staticmethod
@@ -179,12 +191,13 @@ class rotationmap:
         return np.array(labs)[np.argsort(idxs)]
 
     @staticmethod
-    def _populate_dictionary(theta, params):
+    def _populate_dictionary(theta, dictionary_in):
         """Populate the dictionary of free parameters."""
-        for k, key in enumerate(params.keys()):
-            if type(params[key]) is int:
-                params[key] = theta[params[key]]
-                return params
+        dictionary = dictionary_in.copy()
+        for k, key in enumerate(dictionary.keys()):
+            if type(dictionary[key]) is int:
+                dictionary[key] = theta[dictionary[key]]
+        return dictionary
 
     @staticmethod
     def _verify_dictionary(params):
@@ -196,9 +209,13 @@ class rotationmap:
         if params.get('z0') is None:
             params['z0'] = 0.0
         if params.get('psi') is None:
-            params['psi'] = 0.0
+            params['psi'] = 1.0
         if params.get('dist') is None:
             params['dist'] = 100.
+        if params.get('tilt') is None:
+            params['tilt'] = 0.0
+        if params.get('vlsr') is None:
+            params['vlsr'] = 0.0
         if params.get('beam') is None:
             params['beam'] = False
         return params
@@ -302,10 +319,10 @@ class rotationmap:
     # -- Functions to build Keplerian rotation profiles. -- #
 
     def keplerian(self, x0=0.0, y0=0.0, inc=0.0, PA=0.0, z0=0.0, psi=0.0,
-                  nearest='north', mstar=1.0, dist=100., vlsr=0.0):
+                  tilt=0.0, mstar=1.0, dist=100., vlsr=0.0):
         """
-        Return a Keplerian rotation profile (not including pressure). This
-        includes the deivation due to non-zero heights above the midplane,
+        Return a Keplerian rotation profile (not including pressure) in [m/s].
+        This includes the deivation due to non-zero heights above the midplane,
         see Teague et al. (2018a,c) for a thorough description.
 
         Args:
@@ -325,12 +342,23 @@ class rotationmap:
             vproj (ndarray): Projected Keplerian rotation at each pixel [m/s].
         """
         coords = self.disk_coords(x0=x0, y0=y0, inc=inc, PA=PA, z0=z0, psi=psi,
-                                  nearest=nearest, frame='polar')
+                                  tilt=tilt, frame='polar')
         rvals = coords[0] * sc.au * dist
         zvals = coords[2] * sc.au * dist
-        vkep = sc.G * mstar * self.msun * np.power(rvals, 2.0)
+        vkep = sc.G * mstar * 1.988e30 * np.power(rvals, 2.0)
         vkep = np.sqrt(vkep * np.power(np.hypot(rvals, zvals), -3.0))
         return vkep * np.sin(np.radians(inc)) * np.cos(coords[1]) + vlsr
+
+    def _make_model(self, params):
+        """Build the Keplerian model from the dictionary of parameters."""
+        vkep = self.keplerian(x0=params['x0'], y0=params['y0'],
+                              inc=params['inc'], PA=params['PA'],
+                              vlsr=params['vlsr'], z0=params['z0'],
+                              psi=params['psi'], mstar=params['mstar'],
+                              dist=params['dist'], tilt=params['tilt'])
+        if params['beam']:
+            raise NotImplementedError("Woah.")
+        return vkep
 
     # -- Helper functions for loading up the data. -- #
 
@@ -359,19 +387,34 @@ class rotationmap:
             raise ValueError("'a' must be in [0, 1].")
         a_len = self.header['naxis%d' % a]
         a_del = self.header['cdelt%d' % a]
-        if a == 1 and self.absolute:
-            a_del /= np.cos(np.radians(self.header['crval2']))
         a_pix = self.header['crpix%d' % a]
-        a_ref = self.header['crval%d' % a]
-        if not self.absolute:
-            a_ref = 0.0
-            a_pix -= 0.5
-        axis = a_ref + (np.arange(a_len) - a_pix + 1.0) * a_del
-        if self.absolute:
-            return axis
-        return 3600 * axis
+        return 3600 * ((np.arange(a_len) - a_pix + 0.5) * a_del)
 
     # -- Plotting functions. -- #
+
+    def plot_data(self, ax=None, levels=None, ivar=None):
+        """Plot the first moment map."""
+        import matplotlib.cm as cm
+        import matplotlib.pyplot as plt
+        if ax is None:
+            fig, ax = plt.subplots()
+        if levels is None:
+            levels = np.nanpercentile(self.data, [2, 98]) - self.vlsr
+            levels = max(abs(levels[0]), abs(levels[1]))
+            levels = self.vlsr + np.linspace(-levels, levels, 30)
+        im = ax.contourf(self.xaxis, self.yaxis, self.data, levels,
+                         cmap=cm.RdBu_r, extend='both')
+        if ivar is not None:
+            ax.contour(self.xaxis, self.yaxis, ivar, [0], colors='k')
+        cb = plt.colorbar(im, pad=0.02, ticks=np.arange(-20, 20, 0.25))
+        cb.set_label('Line of Sight Velocity (km/s)', rotation=270,
+                     labelpad=15)
+        ax.set_aspect(1)
+        ax.grid(ls=':', color='k', alpha=0.3)
+        ax.set_xlim(self.xaxis.max(), self.xaxis.min())
+        ax.set_ylim(self.yaxis.min(), self.yaxis.max())
+        ax.set_xlabel('Offset (arcsec)')
+        ax.set_ylabel('Offset (arcsec)')
 
     @staticmethod
     def plot_walkers(samples, nburnin=None, labels=None):
