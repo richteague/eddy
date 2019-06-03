4e2bdb1dcfec4df56785e5f407364ced32eae6d2
richteague <richarddteague@gmail.com>
2019-06-03 15:14:00 -0400

    make index master doc

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ autodoc_mock_imports = ['eddy']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
