f641bb9a699ec5c33c36a04b54dae636cd3dc22d
Richard Teague <richardteague@richards-mbp.harnackhaus-berlin.mpg.de>
2022-10-06 16:53:15 +0200

    included default parameters for pip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="astro-eddy",
-    version="2.1.5-5",
+    version="2.1.5-6",
     author="Richard Teague",
     author_email="rteague@mit.edu",
     description=("Tools to recover expectionally precise rotation curves from "
