d9b7128475e2ff2eb8b7db48290d1056b4f30b7d
richteague <rteague@mit.edu>
2023-01-31 09:16:32 -0500

    remove deproject

--- eddy/linecube.py
+++ eddy/linecube.py
@@ -28,15 +28,12 @@ class linecube(datacube):
     # -- ROTATION PROFILE FUNCTIONS -- #
 
     def get_velocity_profile(self, rbins=None, fit_method='GP', fit_vrad=False,
-                             x0=0.0, y0=0.0, inc=0.0, PA=0.0, z0=0.0,
-                             psi=1.0, r_cavity=0.0, r_taper=np.inf,
-                             q_taper=1.0, w_i=None, w_r=None, w_t=None,
-                             z_func=None, shadowed=False, phi_min=None,
-                             phi_max=None, exclude_phi=False, abs_phi=False,
-                             mask_frame='disk', user_mask=None,
-                             beam_spacing=True, deproject=False, niter=1,
-                             get_vlos_kwargs=None, weighted_average=True,
-                             return_samples=False):
+            x0=0.0, y0=0.0, inc=0.0, PA=0.0, z0=0.0, psi=1.0, r_cavity=0.0,
+            r_taper=np.inf, q_taper=1.0, w_i=None, w_r=None, w_t=None,
+            z_func=None, shadowed=False, phi_min=None, phi_max=None,
+            exclude_phi=False, abs_phi=False, mask_frame='disk', user_mask=None,
+            beam_spacing=True, niter=1, get_vlos_kwargs=None,
+            weighted_average=True, return_samples=False):
         """
         Returns the rotational and, optionally, radial velocity profiles under
         the assumption that the disk is azimuthally symmetric (at least across
@@ -99,7 +96,6 @@ class linecube(datacube):
             mask_frame='disk',
             user_mask=None,
             beam_spacing=True,
-            deproject=False,
             niter=1,
             get_vlos_kwargs=None,
             weighted_average=True,
