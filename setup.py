d334b217ddec00aec55211a932668fd147585c91
richteague <rteague@mit.edu>
2024-06-18 11:14:16 +0200

    added pyproject.toml

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="astro-eddy",
-    version="2.4.0",
+    version="2.4.1",
     author="Richard Teague",
     author_email="rteague@mit.edu",
     description=("Tools to study the dynamics of protoplanetary disks."),
